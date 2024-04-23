14d8cfb576da89a4c7aa686d5b7fd46b1a15e4f6
Sam Borkent <sam_borkent@hotmail.com>
2024-04-23 23:06:17 +0200

    feat: ported array changes to circular array

--- circular_array.go
+++ circular_array.go
@@ -3,42 +3,93 @@ package safe
 import (
 	"math"
 	"runtime"
+	"sync"
 	"unsafe"
 )
 
 // A generic fixed size slice with out-of-bounds protection by allowing index overflow.
 // Out-of-bounds indices are wrapped around like in a circular buffer.
-type CircularArray[T any] []T
+type CircularArray[T any] struct {
+	initialized bool
+	array       []T
+	lock        *sync.RWMutex
+}
 
-func NewCircularArray[T any](size int) CircularArray[T] {
-	if size < 0 {
-		size = 0
-	} else if size > math.MaxUint16 {
+func NewCircularArray[T any](length int) *CircularArray[T] {
+	if length <= 0 {
+		// Minimum array length is 1
+		length = 1
+	} else if length > math.MaxUint16 {
+		// Only do a memory check for very large arrays
 		var m runtime.MemStats
 		runtime.ReadMemStats(&m)
 
 		sizeOf := uint64(unsafe.Sizeof(*new(T)))
 
-		if uint64(size)*sizeOf > m.Sys-m.Alloc {
-			size = int((m.Sys - m.Alloc) / sizeOf)
+		if sizeOf == 0 {
+			return &CircularArray[T]{}
+		}
+
+		// TODO: check if this is an accurate way calculate momory available and prevent out of memory panic
+		malloc := (m.Sys - m.Alloc) / sizeOf
+
+		// Limit the length by the maximum memory available
+		if malloc < math.MaxInt && length > int(malloc) {
+			length = int(malloc)
 		}
 	}
 
-	return make(CircularArray[T], size)
+	return &CircularArray[T]{
+		initialized: true,
+		array:       make([]T, length),
+		lock:        new(sync.RWMutex),
+	}
 }
 
-func (a CircularArray[T]) Index(i int) T {
-	return a[a.wrapIndex(i)]
+func (a *CircularArray[T]) Clear() {
+	if !a.initialized {
+		return
+	}
+
+	a.lock.Lock()
+	a.array = make([]T, a.Len())
+	a.lock.Unlock()
 }
 
-func (a CircularArray[T]) Set(i int, value T) {
-	a[a.wrapIndex(i)] = value
+func (a *CircularArray[T]) Index(i int) T {
+	if !a.initialized {
+		return *new(T)
+	}
+
+	a.lock.RLock()
+	defer a.lock.RUnlock()
+	return a.array[a.wrapIndex(i)]
 }
 
-func (a CircularArray[T]) wrapIndex(i int) int {
+func (a *CircularArray[T]) Len() int {
+	if !a.initialized {
+		return 0
+	}
+
+	a.lock.RLock()
+	defer a.lock.RUnlock()
+	return len(a.array)
+}
+
+func (a *CircularArray[T]) Set(i int, value T) {
+	if !a.initialized {
+		return
+	}
+
+	a.lock.Lock()
+	a.array[a.wrapIndex(i)] = value
+	a.lock.Unlock()
+}
+
+func (a *CircularArray[T]) wrapIndex(i int) int {
 	if i < 0 {
-		return len(a) + i
+		return a.Len() + i
 	} else {
-		return i % len(a)
+		return i % a.Len()
 	}
 }
